4425ec6b92e2c5913cf0395ba5f651d2f1fd7bf7
Toph Allen <toph.allen@gmail.com>
2020-07-06 11:10:52 -0400

    Improved version of VibeEditView.

--- Shared/Views/VibeEditView.swift
+++ Shared/Views/VibeEditView.swift
@@ -35,42 +35,113 @@ struct VibeEditView: View {
 
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    OutlineGroup(rootContainers.filter({$0.inVibeTree == true}).first?.childArray ?? [], children: \.childArray) { item in
-                        switch item {
-                        
-                        /// If this is a vibe, we check to see if there's any text in the filter field.
-                        /// If there *is*, we hide any rows that don't match it by returning an EmptyView. Otherwise, we show a VibeEditRow, which contains all the logic for tapping and stuff.
-                        case let vibe as Vibe:
-                            if filterString.count > 0 && !(item.name?.lowercased().contains(filterString.lowercased()) ?? true) {
-                                    EmptyView()
-                                        .frame(maxWidth: .infinity)
-                                } else {
-                                    HStack {
-                                        VibeEditRow(track: track, vibe: vibe)
-                                        Spacer()
-                                    }
-                                        .frame(maxWidth: .infinity)
-                                }
-
-
-                        case let folder as Folder:
-                            HStack {
-                                Text(folder.name ?? "")
-                                    .fontWeight(.bold)
-                                    .lineLimit(1)
-                                    .truncationMode(.middle)
-                                    .padding(.horizontal, 3)
-                                    .padding(.vertical, 0)
-                                Spacer()
-                            }
-                                .frame(maxWidth: .infinity)
-                        default:
-                            EmptyView()
-                                .frame(maxWidth: .infinity)
-                        }
+                    ForEach(rootContainers.filter({$0.inVibeTree == true}).first?.childArray ?? []) { item in
+                        VibeEditRow2(item: item, track: track, filterString: filterString)
                     }
+                    
+                    
+                    
+                    /// This commented out code uses an OutlineGroup, which lets you collapse things but doesn't let them all be expanded at first.
+//                    OutlineGroup(rootContainers.filter({$0.inVibeTree == true}).first?.childArray ?? [], children: \.childArray) { item in
+//                        switch item {
+//
+//                        /// If this is a vibe, we check to see if there's any text in the filter field.
+//                        /// If there *is*, we hide any rows that don't match it by returning an EmptyView. Otherwise, we show a VibeEditRow, which contains all the logic for tapping and stuff.
+//                        case let vibe as Vibe:
+//                            if filterString.count > 0 && !(item.name?.lowercased().contains(filterString.lowercased()) ?? true) {
+//                                    EmptyView()
+//                                        .frame(maxWidth: .infinity)
+//                                } else {
+//                                    HStack {
+//                                        VibeEditRow(track: track, vibe: vibe)
+//                                        Spacer()
+//                                    }
+//                                        .frame(maxWidth: .infinity)
+//                                }
+//
+//
+//                        case let folder as Folder:
+//                            HStack {
+//                                Text(folder.name ?? "")
+//                                    .fontWeight(.bold)
+//                                    .lineLimit(1)
+//                                    .truncationMode(.middle)
+//                                    .padding(.horizontal, 3)
+//                                    .padding(.vertical, 0)
+//                                Spacer()
+//                            }
+//                                .frame(maxWidth: .infinity)
+//                        default:
+//                            EmptyView()
+//                                .frame(maxWidth: .infinity)
+//                        }
+//                    }
                 }
             }
+        }
+    }
+}
+
+
+
+struct VibeEditRowBase: View {
+    var item: Container
+    
+    var body: some View {
+        HStack{
+            Text(item.name ?? "")
+                .lineLimit(1)
+                .truncationMode(.middle)
+                .padding(.horizontal, 3)
+                .padding(.vertical, 1)
+                .truncationMode(.middle)
+            Spacer()
+        }
+    }
+}
+
+
+struct VibeEditRow2: View {
+    @Environment(\.managedObjectContext) var moc
+    @ObservedObject var item: Container
+    @ObservedObject var track: Track
+    var filterString: String
+
+    @ViewBuilder
+    var body: some View {
+        switch item {
+        case let folder as Folder:
+            VibeEditRowBase(item: folder)
+                .font(.headline)
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(folder.childArray ?? []) { item in
+                    VibeEditRow2(item: item, track: track, filterString: filterString)
+                        .padding(.leading, 10)
+                }
+            }
+        case let vibe as Vibe:
+            if filterString.count > 0 && !(item.name?.lowercased().contains(filterString.lowercased()) ?? true) {
+                    EmptyView()
+            } else {
+                if (vibe.tracks?.contains(track)) ?? false {
+                    VibeEditRowBase(item: vibe)
+                        .foregroundColor(Color.white)
+                        .background(Color.accentColor)
+                        .cornerRadius(5.0)
+                        .onTapGesture {
+                            vibe.removeFromTracks(track)
+                            try! moc.save()
+                        }
+                } else {
+                    VibeEditRowBase(item: vibe)
+                        .onTapGesture {
+                            vibe.addToTracks(track)
+                            try! moc.save()
+                        }
+                }
+            }
+        default:
+            EmptyView()
         }
     }
 }
